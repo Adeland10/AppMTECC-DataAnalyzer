bff4ae7ed54b72e4381eedeed242de09d007b936
Adeland10 <adele.faille@live.fr>
2024-07-18 15:25:51 +0200

    Création d'un environnement virtuel

--- par_puits/main_par_puits.py
+++ par_puits/main_par_puits.py
@@ -6,7 +6,7 @@ from plotting import plot_histo_par_puits
 from table import create_delta_table
 
 
-#filepath = r'C:\Users\adele\Bureau\INEM\CODE\Code-MTECC\HNE PredictCFdec162k p1+3F508del and CastanierSoleneWT p1+3 and no cells in 5 6 20 06 24_comp.xlsx'
+filepath = r'C:\Users\adele\Bureau\INEM\CODE\Code-MTECC\ReadyToAnalyze\HNE PredictCFdec162k p1+3F508del and CastanierSoleneWT p1+3 and no cells in 5 6 20 06 24_comp.xlsx'
 #sheet = 'HNE PredictCFdec162k p1+3F508de'
 df = pd.read_excel(filepath)
     
